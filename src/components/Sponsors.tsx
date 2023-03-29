99220fdebda1f208594bd6d2e63aeb69de8c2c2b
Geoffrey Rekier <112507798+agilxpgeoffrey@users.noreply.github.com>
2023-03-29 18:06:47 +0200

    Update Sponsors.tsx

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -23,7 +23,7 @@ const Sponsors = () => {
         <div className="bg-white dark:bg-gray-900 py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <h2 className="text-center text-lg font-semibold leading-8 text-gray-900 dark:text-white">
-                    Our proud sponsors
+                    Sponsored by
                 </h2>
                 <div
                     className=" flex flex-row justify-center mx-auto mt-5 py-5 max-w-lg items-center gap-x-8 gap-y-10 sm:max-w-xl sm:gap-x-10 lg:mx-0 lg:max-w-none overflow-y-scroll">
