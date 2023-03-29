4e077570dd795bed760fe79d3337844c8ebba7e0
Geoffrey Rekier <geoffrey@agilxp.com>
2023-03-29 14:04:14 +0200

    Remove unused

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -1,7 +1,4 @@
-import { createClient } from 'next-sanity'
-import { clientConfig, previewConfig } from '@/sanity.config'
+import { previewConfig } from '@/sanity.config'
 import { definePreview } from '@sanity/preview-kit'
-
-export const client = createClient(clientConfig)
 
 export const usePreview = definePreview(previewConfig)
