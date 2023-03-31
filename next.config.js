cc0db59f78ccbf28ff366600f7e63672256c2d48
Geoffrey Rekier <geoffrey@agilxp.com>
2023-03-31 17:54:03 +0200

    More cleanup

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    dangerouslyAllowSVG: true,
-    remotePatterns: [
-      {hostname: 'cdn.sanity.io'},
-    ]
-  }
 }
 
 module.exports = nextConfig
