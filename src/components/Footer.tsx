b67267244019b60750223a52a7c2e141eea26a60
Geoffrey Rekier <geoffrey@agilxp.com>
2023-04-01 11:10:45 +0200

    Hide social icons in footer which we do not have (yet?)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,6 +11,7 @@ const navigation = [
                 />
             </svg>
         ),
+        visible: false,
     },
     {
         name: 'Instagram',
@@ -24,6 +25,7 @@ const navigation = [
                 />
             </svg>
         ),
+        visible: false,
     },
     {
         name: 'Twitter',
@@ -33,6 +35,7 @@ const navigation = [
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
             </svg>
         ),
+        visible: false,
     },
     {
         name: 'GitHub',
@@ -46,6 +49,7 @@ const navigation = [
                 />
             </svg>
         ),
+        visible: true,
     },
     {
         name: 'YouTube',
@@ -59,6 +63,7 @@ const navigation = [
                 />
             </svg>
         ),
+        visible: false,
     },
 ]
 
@@ -67,7 +72,7 @@ const Footer = () => {
         <footer className="fixed bottom-0 left-0 z-20 w-full border-t-2 bg-white dark:bg-gray-900">
             <div className="mx-auto max-w-7xl p-6 md:flex md:items-center md:justify-between lg:px-8">
                 <div className="flex justify-center space-x-6 md:order-2">
-                    {navigation.map((item) => (
+                    {navigation.filter((item) => item.visible).map((item) => (
                         <a key={item.name} href={item.href} target='_blank' className="text-gray-400 hover:text-gray-500 dark:text-white dark:hover:text-gray-300">
                             <span className="sr-only">{item.name}</span>
                             <item.icon className="h-6 w-6" aria-hidden="true" />
